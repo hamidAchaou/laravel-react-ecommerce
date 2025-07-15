69ec959f29c72fcdca3f307c36035e1d596955d7
Hamid Achaou <achaou.hamid.solicode@gmail.com>
2025-07-15 16:46:40 +0100

    applicated daisyui

--- app/resources/js/Components/TextInput.tsx
+++ app/resources/js/Components/TextInput.tsx
@@ -4,38 +4,36 @@ import {
     useEffect,
     useImperativeHandle,
     useRef,
-} from 'react';
-
-export default forwardRef(function TextInput(
+  } from 'react';
+  
+  export default forwardRef(function TextInput(
     {
-        type = 'text',
-        className = '',
-        isFocused = false,
-        ...props
+      type = 'text',
+      className = '',
+      isFocused = false,
+      ...props
     }: InputHTMLAttributes<HTMLInputElement> & { isFocused?: boolean },
     ref,
-) {
+  ) {
     const localRef = useRef<HTMLInputElement>(null);
-
+  
     useImperativeHandle(ref, () => ({
-        focus: () => localRef.current?.focus(),
+      focus: () => localRef.current?.focus(),
     }));
-
+  
     useEffect(() => {
-        if (isFocused) {
-            localRef.current?.focus();
-        }
+      if (isFocused) {
+        localRef.current?.focus();
+      }
     }, [isFocused]);
-
+  
     return (
-        <input
-            {...props}
-            type={type}
-            className={
-                'rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 ' +
-                className
-            }
-            ref={localRef}
-        />
+      <input
+        {...props}
+        ref={localRef}
+        type={type}
+        className={`input input-bordered w-full focus:outline-none focus:ring-2 focus:ring-primary ${className}`}
+      />
     );
-});
+  });
+  
